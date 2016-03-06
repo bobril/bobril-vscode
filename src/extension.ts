31f46ffe9f6303acc637247babf41390eb65b29a
Boris Letocha <boris.letocha@gmail.com>
2016-03-06 02:16:14 +0100

    First version of Bobril extension.

---
+++ src/extension.ts
@@ -0,0 +1,184 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as longPollingClient from './longPollingClient';
+
+export interface StackFrame {
+    functionName?: string;
+    args?: any[];
+    fileName?: string;
+    lineNumber?: number;
+    columnNumber?: number;
+}
+
+export interface SuiteOrTest {
+    isSuite: boolean;
+    name: string;
+    skipped: boolean;
+    failure: boolean;
+    duration: number;
+    failures: { message: string, stack: StackFrame[] }[];
+    nested: SuiteOrTest[];
+}
+
+export interface TestResultsHolder extends SuiteOrTest {
+    userAgent: string;
+    running: boolean;
+    testsFailed: number;
+    testsSkipped: number;
+    testsFinished: number;
+    totalTests: number;
+}
+
+export interface TestSvrState {
+    agents: TestResultsHolder[];
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Bobril companion is now active!');
+
+    let c = new longPollingClient.Connection('http://localhost:8080/bb/api/main');
+
+    let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
+    let connected = false;
+    let disconnected = false;
+    let reconnectDelay = 0;
+    let compilationStatus = "";
+    let testStatus = "";
+
+    function updateStatusBar() {
+        let s = "Disconnected";
+        if (connected) {
+            s = "Connected";
+            if (compilationStatus || testStatus)
+                s = compilationStatus + testStatus;
+        } else if (!disconnected) {
+            s = "Connecting";
+        }
+        statusBarItem.text = s;
+        statusBarItem.show();
+    }
+
+    function reconnect() {
+        disconnected = false;
+        c.connect();
+        updateStatusBar();
+    }
+
+    c.onClose = () => {
+        connected = false;
+        disconnected = true;
+        updateStatusBar();
+        if (reconnectDelay < 30000) reconnectDelay += 1000;
+        setTimeout(() => {
+            reconnect();
+        }, reconnectDelay);
+    };
+
+    c.onMessage = (c: longPollingClient.Connection, message: string, data: any) => {
+        if (!connected) {
+            connected = true;
+            updateStatusBar();
+        }
+        switch (message) {
+            case "compilationStarted": {
+                compilationStatus = "Compiling";
+                updateStatusBar();
+                break;
+            }
+            case "compilationFinished": {
+                compilationStatus = "E:" + data.errors + " W:" + data.warnings + " " + data.time + "ms";
+                updateStatusBar();
+                break;
+            }
+            case "testUpdated": {
+                let state = data as TestSvrState;
+                testStatus = "";
+                state.agents.forEach(a=> {
+                    testStatus += " F:" + a.testsFailed + "/" + a.totalTests + " " + a.duration.toFixed(0) + "ms";
+                });
+                updateStatusBar();
+                break;
+            }
+            default: {
+                console.log("Unknown message: " + message, data);
+                break;
+            }
+        }
+    };
+
+    reconnect();
+    
+    // create a decorator type that we use to decorate small numbers
+    var consoleLogDecorationType = vscode.window.createTextEditorDecorationType({
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        overviewRulerColor: 'blue',
+        overviewRulerLane: vscode.OverviewRulerLane.Right,
+        light: {
+            // this color will be used in light color themes
+            borderColor: 'darkblue'
+        },
+        dark: {
+            // this color will be used in dark color themes
+            borderColor: 'lightblue'
+        }
+    });
+
+    var activeEditor = vscode.window.activeTextEditor;
+    if (activeEditor) {
+        triggerUpdateDecorations();
+    }
+
+    vscode.window.onDidChangeActiveTextEditor(editor => {
+        activeEditor = editor;
+        if (editor) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
+
+    vscode.workspace.onDidChangeTextDocument(event => {
+        if (activeEditor && event.document === activeEditor.document) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
+
+    var timeout = null;
+    function triggerUpdateDecorations() {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(updateDecorations, 500);
+    }
+
+    function updateDecorations() {
+        if (!activeEditor) {
+            return;
+        }
+        var regEx = /\d+/g;
+        var text = activeEditor.document.fileName;
+        var smallNumbers: vscode.DecorationOptions[] = [];
+        var match;
+        while (match = regEx.exec(text)) {
+            var startPos = activeEditor.document.positionAt(match.index);
+            var endPos = activeEditor.document.positionAt(match.index + match[0].length);
+            var decoration = <vscode.DecorationOptions>{
+                range: new vscode.Range(startPos, endPos),
+                hoverMessage: 'Number **' + match[0] + '**'
+            };
+            if (match[0].length < 3) {
+                smallNumbers.push(decoration);
+            }
+
+        }
+        activeEditor.setDecorations(consoleLogDecorationType, smallNumbers);
+    }
+
+    context.subscriptions.push(consoleLogDecorationType);
+}
+	
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
---
+++ src/longPollingClient.ts
@@ -0,0 +1,182 @@
+import * as url from "url";
+import * as http from "http";
+
+export class Connection {
+    private url: string;
+    private id: string;
+    private toSend: any[];
+    private sendTimer: number;
+    private closed: boolean;
+    private longPolling: http.ClientRequest;
+    private heartBeatTimer: number;
+
+    onMessage: (connection: Connection, message: string, data: any) => void;
+    onClose: (connection: Connection) => void;
+
+    constructor(url: string) {
+        this.onMessage = null;
+        this.onClose = null;
+        this.url = url;
+        this.toSend = [];
+        this.id = "";
+        this.sendTimer = -1;
+        this.closed = false;
+        this.longPolling = null;
+        this.heartBeatTimer = -1;
+    }
+
+    connect() {
+        this.toSend = [];
+        this.id = "";
+        this.sendTimer = -1;
+        this.closed = false;
+        this.longPolling = null;
+        this.heartBeatTimer = -1;
+        this.reSendTimer();
+    }
+
+    send(message: string, data: any) {
+        this.toSend.push({ m: message, d: data });
+        this.reSendTimer();
+    }
+
+    close() {
+        if (this.closed)
+            return;
+        if (this.longPolling) {
+            this.longPolling.abort();
+            this.longPolling = null;
+        }
+        this.closed = true;
+        this.toSend = [];
+        if (this.onClose != null)
+            this.onClose(this);
+        this.reSendTimer();
+    }
+
+    private reSendTimer() {
+        if (this.sendTimer === -1 && (!this.closed || this.id != "")) {
+            this.sendTimer = <any>setTimeout(() => {
+                this.doSend();
+            }, 10);
+        }
+    }
+
+    private parseResponse(resp: string): boolean {
+        var data;
+        try {
+            data = JSON.parse(resp);
+        } catch (err) {
+            return false;
+        }
+        if (typeof data.id !== "string") {
+            return false;
+        }
+        this.id = data.id;
+        if (data.close) {
+            return false;
+        }
+        let m = data.m;
+        if (Array.isArray(m)) {
+            for (let i = 0; i < m.length; i++) {
+                this.onMessage(this, m[i].m, m[i].d);
+            }
+        }
+        return true;
+    }
+
+    private doSend() {
+        this.sendTimer = -1;
+        if (this.closed && this.id === '')
+            return;
+        let cliReqOpt = <any>url.parse(this.url);
+        cliReqOpt.method = "POST";
+        cliReqOpt.headers = {
+            "Connection": "keep-alive"
+        }
+        var req = http.request(cliReqOpt, (res=> {
+            //console.log(`STATUS: ${res.statusCode}`);
+            //console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+                this.close();
+                req.abort();
+                return;
+            }
+            res.setEncoding('utf8');
+            let resp = "";
+            res.on('data', (chunk) => {
+                //console.log(`BODY: ${chunk}`);
+                resp += chunk;
+            });
+            res.on('end', () => {
+                if (!this.parseResponse(resp)) {
+                    this.id = '';
+                    this.close();
+                    return;
+                }
+                if (!this.longPolling)
+                    this.startLongPolling();
+                this.startHeartBeat();
+            })
+        }));
+        req.on('error', (e) => {
+            this.close();
+        });
+        req.write(JSON.stringify(this.closed ? { id: this.id, close: true } : { id: this.id, m: this.toSend }));
+        req.end();
+        if (this.closed)
+            this.id = "";
+        this.toSend = [];
+    }
+
+    private startLongPolling() {
+        if (this.closed || this.id === '')
+            return;
+        let cliReqOpt = <any>url.parse(this.url);
+        cliReqOpt.method = "POST";
+        cliReqOpt.headers = {
+            "Connection": "keep-alive"
+        }
+        var req = http.request(cliReqOpt, (res=> {
+            //console.log(`LSTATUS: ${res.statusCode}`);
+            //console.log(`LHEADERS: ${JSON.stringify(res.headers)}`);
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+                this.longPolling = null;
+                this.startLongPolling();
+                return;
+            }
+            res.setEncoding('utf8');
+            let resp = "";
+            res.on('data', (chunk) => {
+                //console.log(`LBODY: ${chunk}`);
+                resp += chunk;
+            });
+            res.on('end', () => {
+                this.longPolling = null;
+                if (!this.parseResponse(resp)) {
+                    this.id = '';
+                    this.close();
+                    return;
+                }
+                this.startLongPolling();
+                this.startHeartBeat();
+            })
+        }));
+        req.on('error', (e) => {
+            this.longPolling = null;
+            this.close();
+        });
+        req.end(JSON.stringify({ id: this.id }));
+        this.longPolling = req;
+    }
+
+    private startHeartBeat() {
+        if (this.heartBeatTimer != -1) {
+            clearTimeout(this.heartBeatTimer);
+        }
+        this.heartBeatTimer = setTimeout(() => {
+            this.heartBeatTimer = -1;
+            this.doSend();
+        }, 30000);
+    }
+}
